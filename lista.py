7332b26a372a94a52f49c7d827d07bd4c86b6f35
Leonardo Lima <160889904+leojesusz@users.noreply.github.com>
2024-04-23 10:02:15 -0300

    Add files via upload

---
+++ lista.py
@@ -0,0 +1,11 @@
+lista = [
+    "leo", "ryan", "levi"
+]
+lista.insert(1, "dias")
+print(lista)
+
+objeto = [
+    {"nome": "leo", "idade": 20},
+    {"nome": "ryan", "idade": 18}
+]
+print(objeto[0])
